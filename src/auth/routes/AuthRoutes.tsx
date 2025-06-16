2fff7767090c0ba7159b13e5bb595881e4ee274e
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-15 19:30:44 -0500

    routes to pages added

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,9 +1,15 @@
-import { Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { SigninPage } from "../pages/SigninPage"
+import { SignupPage } from "../pages/SignupPage"
 
 export const AuthRoutes = () => {
   return (
     <Routes>
-      
+      <Route path="signin" element={ <SigninPage /> } />
+
+      <Route path="signup" element={ <SignupPage /> } />
+
+      <Route path="/*" element={ <Navigate to={"/auth/login"} /> } />
     </Routes>
   )
 }
