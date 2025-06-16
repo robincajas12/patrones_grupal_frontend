713018bbcf7a8e497e0a75ef1db02ca885c34811
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-15 19:25:29 -0500

    auth routes component added

---
+++ src/auth/routes/AuthRoutes.tsx
@@ -0,0 +1,9 @@
+import { Routes } from "react-router-dom"
+
+export const AuthRoutes = () => {
+  return (
+    <Routes>
+      
+    </Routes>
+  )
+}
--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -1,9 +1,19 @@
 import { Routes } from "react-router-dom"
+import { useAuthStore } from "../store/auth/hooks/useAuthStore"
 
 export const AppRouter = () => {
+
+  const status = useAuthStore(( state ) => state.status)
+
+  if(status === 'checking') {
+    return <div>Loading...</div>
+  }
+
   return (
     <Routes>
-      <div>Main router</div>
+      {/* {
+        status === 'authenticated' ? 
+      } */}
     </Routes>
   )
 }
