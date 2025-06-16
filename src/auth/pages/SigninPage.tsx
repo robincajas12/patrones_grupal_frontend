74c21e9e7e39d21433673f5c43da35eed03ae414
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-15 21:47:01 -0500

    signin buttons added

--- src/auth/pages/SigninPage.tsx
+++ src/auth/pages/SigninPage.tsx
@@ -3,7 +3,17 @@ import { AuthLayout } from "../layout/AuthLayout"
 export const SigninPage = () => {
   return (
     <AuthLayout title="Signin">
-      <div>SigninPage</div>
+      <div className="auth-container">
+        <form className="auth-form">
+          <input type="email" id="email" name="email" placeholder="email" required />
+          <input type="password" id="password" name="password" placeholder="password" required />
+          
+          <div className="auth-buttons">
+            <button type="submit">Sign In</button>
+            <button type="button" className="auth-button auth-button--google">Google</button>
+          </div>
+        </form>
+      </div>
     </AuthLayout>
   )
 }
