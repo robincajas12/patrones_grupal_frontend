6fb05105d4a9d9dba5725364d6b9d787e7ff0647
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-15 19:03:12 -0500

    auth methods mock implementation

--- src/store/auth/hooks/useAuthStore.ts
+++ src/store/auth/hooks/useAuthStore.ts
@@ -1,3 +1,4 @@
+import { create } from "zustand";
 import type { User } from "../interfaces/User"
 import { SignupFormData } from '../interfaces/signup-form-data.interface';
 
@@ -19,3 +20,31 @@ interface AuthState {
   currentUser: (user: User) => Promise<void>
 }
 
+export const useAuthStore = create<AuthState>()((set, get) => ({
+  status: 'checking',
+  user: null,
+
+  signin: async (email: string, password: string) => {
+    return true
+  },
+
+  socialSignin: async (provider: string) => {
+    return true
+  },
+  signup: async (signupFormData: SignupFormData) => {
+    return true
+  },
+  logout: async () => {
+    set({ status: 'unauthenticated', user: null })
+  },
+  changeStatus: async () => {
+    const currentStatus = get().status;
+    set({ status: currentStatus === 'checking' ? 'unauthenticated' : 'checking' });
+    return true;
+  }
+  ,
+  currentUser: async (user: User) => {
+    set({ user, status: 'authenticated' });
+    return;
+  }
+}))
