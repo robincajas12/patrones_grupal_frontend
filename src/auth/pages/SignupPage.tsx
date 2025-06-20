4fbd9a4f427f2d40214e12df756ae9ea0b08a188
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-19 21:53:11 -0500

    signup page added

--- src/auth/pages/SignupPage.tsx
+++ src/auth/pages/SignupPage.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom"
+
+import { AuthLayout } from "../layout/AuthLayout"
+
 export const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <AuthLayout title="Signup">
+      <div className="auth-container">
+        <form className="auth-form">
+          <input type="text" id="name" name="name" placeholder="name" required />
+          <input type="email" id="email" name="email" placeholder="email" required />
+          <input type="password" id="password" name="password" placeholder="password" required />
+          
+          <div className="auth-buttons">
+            <button type="submit">Create Account</button>
+          </div>
+        </form>
+
+        <div className="auth-footer">
+          <Link className="auth-footer-link" to="/auth/signin">Already have an account? Sign in</Link>
+        </div>
+      </div>
+    </AuthLayout>
   )
 }
