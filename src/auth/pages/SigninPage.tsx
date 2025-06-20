889b8659ca2a41ec87f56bf8cc56d74ababc2fe2
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-19 22:10:47 -0500

    using use form for form state

--- src/auth/pages/SigninPage.tsx
+++ src/auth/pages/SigninPage.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
 
 import { AuthLayout } from "../layout/AuthLayout"
+import { useForm } from "../hooks/useForm"
 
 export const SigninPage = () => {
+
+  const { initialState, onFormChange } = useForm({ email: 'hooa', password: '' })
+
   return (
     <AuthLayout title="Iniciar Sesion">
       <div className="auth-container">
         <form className="auth-form">
-          <input type="email" id="email" name="email" placeholder="Correo Electronico" required />
-          <input type="password" id="password" name="password" placeholder="Contraseña" required />
+          <input type="email" onChange={ onFormChange } value={ initialState.email } id="email" name="email" placeholder="Correo Electronico" required />
+          <input type="password" onChange={ onFormChange } value={ initialState.password } id="password" name="password" placeholder="Contraseña" required />
 
           <div className="auth-buttons">
             <button type="submit">INICIAR SESION</button>
