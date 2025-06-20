ec8b8d15da57cec3d8c59b945c9626905487ce34
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-19 22:06:17 -0500

    use form hook created

---
+++ src/auth/hooks/useForm.ts
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export const useForm = <T>(initialValues: T) => {
+  const [initialState, setInitialState] = useState(initialValues)
+
+  const onFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setInitialState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  return {
+    initialState,
+    onFormChange,
+  }
+}
