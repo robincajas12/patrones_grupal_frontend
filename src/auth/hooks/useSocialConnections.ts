f955475f5128b29b760f1f3279d63c1f5ef45086
Mateo Curicho <mateocuricho28@gmail.com>
2025-06-22 16:05:02 -0500

    google connection with popup added

--- src/auth/hooks/useSocialConnections.ts
+++ src/auth/hooks/useSocialConnections.ts
@@ -1,8 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react"
+
 export const useSocialConnections = () => {
 
-  
+  const { loginWithPopup, user, isLoading, isAuthenticated } = useAuth0()
+
+  const startGoogleConnection = async () => {
+    try {
+      await loginWithPopup({
+        authorizationParams: {
+          connection: "google-oauth2",
+          prompt: "select_account",
+          scope: "openid profile email",
+        }
+      })
+
+      console.log({ user, isLoading, isAuthenticated });
+    } catch (error) {
+      console.error("Error starting Google connection:", error)
+    }
+  }
 
   return {
-
+    startGoogleConnection
   }
 }
