ded09807e250732daa90488678f2933ec97547ce
Mateo Curicho <mateocuricho28@gmail.com>
2025-06-22 16:36:29 -0500

    using store to signin with social

--- src/auth/pages/SigninPage.tsx
+++ src/auth/pages/SigninPage.tsx
@@ -3,15 +3,40 @@ import { Link } from "react-router-dom"
 import { AuthLayout } from "../layout/AuthLayout"
 import { useForm } from "../hooks/useForm"
 import { useSocialConnections } from "../hooks/useSocialConnections"
+import { useAuthStore } from "../../store/auth/hooks/useAuthStore"
+import type { User } from "../../store/auth/interfaces/User"
 
 export const SigninPage = () => {
 
   const { initialState, onFormChange } = useForm({ email: '', password: '' })
+
+  const { socialSignin } = useAuthStore()
   
   const { startGoogleConnection, user, isAuthenticated, isLoading } = useSocialConnections()
 
   console.log({ user, isAuthenticated, isLoading });
+
+  const onGoogleSignin = async () => {
+    await startGoogleConnection()
+
+    if(!user) {
+      console.error("No user found after Google sign-in")
+      return
+    }
+
+    await socialSignin({ email: user.email, name: user.given_name, id: user.sub } as User)
+
+  }
   
+  if( isLoading ) {
+    return (
+      <AuthLayout title="Cargando...">
+        <div className="auth-container">
+          <p>Cargando...</p>
+        </div>
+      </AuthLayout>
+    )
+  }
 
   return (
     <AuthLayout title="Iniciar Sesion">
