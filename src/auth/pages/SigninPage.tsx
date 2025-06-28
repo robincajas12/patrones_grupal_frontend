a69e083c5f1d824e565f4f67535e36a537b9407c
Mateo Curicho <mateocuricho28@gmail.com>
2025-06-28 13:48:10 -0500

    error message shown

--- src/auth/pages/SigninPage.tsx
+++ src/auth/pages/SigninPage.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react"
+
 import { Link } from "react-router-dom"
 
 import { AuthLayout } from "../layout/AuthLayout"
@@ -5,11 +7,16 @@ import { useForm } from "../hooks/useForm"
 import { useSocialConnections } from "../hooks/useSocialConnections"
 import { useAuthStore } from "../../store/auth/hooks/useAuthStore"
 import type { User } from "../../store/auth/interfaces/user.interface"
-import { useEffect } from "react"
+import { validateEmail, validatePassword } from "../utils/validate-form-fields"
 
 export const SigninPage = () => {
 
   const { initialState, onFormChange } = useForm({ email: '', password: '' })
+
+  const [isEmailValid, setIsEmailValid] = useState( true )
+
+  const [isPasswordValid, setIsPasswordValid] = useState( true )
+
 
   const { socialSignin } = useAuthStore()
   
@@ -20,6 +27,20 @@ export const SigninPage = () => {
     onGoogleSignin({ email: auth0User?.email || '', name: auth0User?.name || '', id: auth0User?.sub || '' })
 
   }, [isAuthenticated])
+
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setIsEmailValid(validateEmail(initialState.email))
+
+    setIsPasswordValid(validatePassword(initialState.password))
+
+    if(!isEmailValid || !isPasswordValid) {
+      return
+    }
+
+    console.log(initialState);
+  }
 
   const onGoogleSignin = async (user?: User) => {
     if(!user || user?.email === '') {
@@ -47,12 +68,14 @@ export const SigninPage = () => {
   return (
     <AuthLayout title="Iniciar Sesion">
       <div className="auth-container">
-        <form className="auth-form">
-          <input autoComplete="email" type="email" onChange={ onFormChange } value={ initialState.email } id="email" name="email" placeholder="Correo Electronico" required />
-          <input autoComplete="current-password" type="password" onChange={ onFormChange } value={ initialState.password } id="password" name="password" placeholder="Contraseña" required />
+        <form className="auth-form" onSubmit={ onFormSubmit }>
+          <input style={{ borderColor: isEmailValid ? 'black' : 'red' }} autoComplete="email" type="text" onChange={ onFormChange } value={ initialState.email } id="email" name="email" placeholder="Correo Electronico" required />
+          {!isEmailValid && <p className="error-field">El correo electrónico no es válido</p>}
+          <input style={{ borderColor: isPasswordValid ? 'black' : 'red' }} autoComplete="current-password" type="password" onChange={ onFormChange } value={ initialState.password } id="password" name="password" placeholder="Contraseña" required />
+          {!isPasswordValid && <p className="error-field">La contraseña debe tener al menos 6 caracteres</p>}
 
           <div className="auth-buttons">
-            <button onClick={ () => console.log(auth0User)} type="submit">INICIAR SESION</button>
+            <button type="submit">INICIAR SESION</button>
             <button onClick={startGoogleConnection} type="button">GOOGLE</button>
           </div>
         </form>
