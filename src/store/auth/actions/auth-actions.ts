a084880970a780b8bf25286a413cac76c8818ba7
Mateo Curicho <mateocuricho28@gmail.com>
2025-06-28 18:40:10 -0500

    error catched in request

--- src/store/auth/actions/auth-actions.ts
+++ src/store/auth/actions/auth-actions.ts
@@ -3,12 +3,17 @@ import { api } from "../../../core/api/app-axios"
 import type { User } from "../interfaces/user.interface";
 
 export const authSignin = async (email: string, password: string): Promise<User | null> => {
-
   email = email.trim().toLowerCase()
 
   password = password.trim()
-  
-  const response = await api.post<User>('/auth/signin', { email, password })
 
-  return response.data
+  try {
+    const response = await api.post<User>('/auth/signin', { email, password })
+    
+    return response.data
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+
 }
